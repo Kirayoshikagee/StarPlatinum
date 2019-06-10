5ed06a4700530f6969539c113bb3d2b14dcffea2
Kirayoushikagee <2491412218@qq.com>
2019-06-10 10:49:44 +0800

    测试AssetBundel加载

--- ArtFramework/Assets/Script/TestLoadAssetBundle.cs
+++ ArtFramework/Assets/Script/TestLoadAssetBundle.cs
@@ -7,14 +7,39 @@ using UnityEngine;
 public class TestLoadAssetBundle : MonoBehaviour {
 
     GameObject loadingPrefab;
+    GameObject copyLoadingPrefab;
 
     void Awake()
     {
-        StartCoroutine(Load());
+        StartCoroutine(LoadStandardAsset());
+    }
+
+    IEnumerator LoadAllAssets() {
+        string streamingAssetPath = Path.Combine("Assets/StreamingAssets/Windows", "Windows");
+        streamingAssetPath = streamingAssetPath.Replace("\\", "/");
+        AssetBundle allBundles = AssetBundle.LoadFromFile(streamingAssetPath);
+        yield return allBundles;
+
+        if (allBundles == null) {
+            Debug.LogError("Failed to load all bundles");
+            yield break;
+        }
+
+        string[] allBundleNames = allBundles.GetAllAssetNames();
+
+        for (int i = 0; i < allBundleNames.Length; i++) {
+            Debug.LogError("Bundle:" + allBundleNames[i]);
+        }
+
+        if (allBundles != null) {
+
+        }
+
+        yield return null;
     }
 
     // Use this for initialization
-    IEnumerator Load()
+    IEnumerator LoadStandardAsset()
     {
         //assets$prefab$panelloading.unity3d
         string streamingAssetPath = Path.Combine("Assets/StreamingAssets/Windows", "assets$prefab$panelloading.unity3d");
@@ -31,12 +56,19 @@ public class TestLoadAssetBundle : MonoBehaviour {
 
         string[] allAssets = bundleLoadRequest.GetAllAssetNames();
 
+        GameObject template = null;
+
         for (int i = 0; i < allAssets.Length; i++) {
             Debug.LogError("Assets:" + allAssets[i]);
-            GameObject asset = bundleLoadRequest.LoadAsset<GameObject>(allAssets[i]);
-
-            loadingPrefab = GameObject.Instantiate(asset) as GameObject;
+            template = bundleLoadRequest.LoadAsset<GameObject>(allAssets[i]);
+            loadingPrefab = GameObject.Instantiate(template) as GameObject;
+        
             loadingPrefab.transform.SetParent(transform);
+            loadingPrefab.transform.localPosition = Vector3.zero;
+            RectTransform loadingRect = loadingPrefab.GetComponent<RectTransform>();
+            loadingRect.anchorMax = Vector2.zero;
+            loadingRect.anchorMin = Vector2.zero;
+            loadingRect.sizeDelta = new Vector2(100, 200);
         }
 
 
@@ -44,6 +76,20 @@ public class TestLoadAssetBundle : MonoBehaviour {
             bundleLoadRequest.Unload(false);
             bundleLoadRequest = null;
         }
+
+        if (template != null) {
+            Debug.LogError("template is not null");
+            loadingPrefab = GameObject.Instantiate(template) as GameObject;
+            loadingPrefab.name = "copyLoading";
+
+            loadingPrefab.transform.SetParent(transform);
+            loadingPrefab.transform.localPosition = Vector3.zero;
+            RectTransform loadingRect = loadingPrefab.GetComponent<RectTransform>();
+            loadingRect.anchorMax = Vector2.zero;
+            loadingRect.anchorMin = Vector2.zero;
+            loadingRect.sizeDelta = new Vector2(200, 200);
+        }
+        
     }
 	
 	// Update is called once per frame
